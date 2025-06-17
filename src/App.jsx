25b96c19019468c2a3f9584703d49ac3e92ca439
Jaswanth <jaswanth2310325@ssn.edu.in>
2025-06-18 01:28:24 +0530

    student home comp and current gpa calc comp added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Profile from "./projectComponents/Profile.jsx";
 import Navbar from "./projectComponents/Navbar.jsx";
+import StudentHomepageSubject from "./projectComponents/studentHomepageSubject.jsx";
+import GPAcalccurrent from "./projectComponents/GPAcalccurrent.jsx";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
     </>
   );
 }
