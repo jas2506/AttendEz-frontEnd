3466775edf6defd90a261895de63abf1582a1156
Jaswanth <jaswanth2310325@ssn.edu.in>
2025-06-20 18:58:03 +0530

    jas components added

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,10 @@ import Navbar from "./projectComponents/Navbar.jsx";
 import StudentHomepageSubject from "./projectComponents/studentHomepageSubject.jsx";
 import GPAcalccurrent from "./projectComponents/GPAcalccurrent.jsx";
 import GPAForm from "./projectComponents/GPAForm.jsx";
-import StudentAttendanceTable from "./projectComponents/StudentAttendanceTable.jsx"; 
+import StudentAttendanceTable from "./projectComponents/StudentAttendanceTable.jsx";
 
 function App() {
-
- 
-
-  return (
-    <>
-
-    </>
-  );
+  return <></>;
 }
 
 export default App;
